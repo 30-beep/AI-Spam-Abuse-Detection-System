2db66cdc6cb971bfd286035e94b141263b932cd4
30-beep <157449685+30-beep@users.noreply.github.com>
2026-03-06 20:58:55 +0800

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,1 +1,39 @@
+from flask import Flask, render_template, request, redirect
+from ai_detector import analyze_comment
 
+app = Flask(__name__)
+
+comments = []
+flagged_comments = []
+
+@app.route('/')
+def login():
+    return render_template("login.html")
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template("dashboard.html", comments=comments)
+
+@app.route('/post_comment', methods=['POST'])
+def post_comment():
+
+    text = request.form['comment']
+    result = analyze_comment(text)
+
+    comment_data = {
+        "text": text,
+        "status": result
+    }
+
+    comments.append(comment_data)
+
+    if result != "Safe":
+        flagged_comments.append(comment_data)
+
+    return redirect("/dashboard")
+
+@app.route('/moderator')
+def moderator():
+    return render_template("moderator.html", comments=flagged_comments)
+
+app.run(debug=True)
